abc0201be966608b4ef520410a66debcb2aa6e2d
Optimusthy.dev <141915285+Vuthyy@users.noreply.github.com>
2024-07-04 09:53:30 +0700

    Update index-03.js

--- index-03.js
+++ index-03.js
@@ -1,21 +1,17 @@
-function getLetterGrade(numericalGrade) {
-  if (numericalGrade >= 90 && numericalGrade <= 100) {
-    return "A";
-  } else if (numericalGrade >= 80 && numericalGrade < 90) {
-    return "B";
-  } else if (numericalGrade >= 70 && numericalGrade < 80) {
-    return "C";
-  } else if (numericalGrade >= 60 && numericalGrade < 70) {
-    return "D";
-  } else if (numericalGrade >= 50 && numericalGrade < 60) {
-    return "E";
-  } else if (numericalGrade >= 0 && numericalGrade < 50) {
-    return "F";
-  } else {
-    return "Invalid grade";
-  }
+let numericalGrade = 95;
+
+if (numericalGrade >= 90 && numericalGrade <= 100) {
+  console.log("A");
+} else if (numericalGrade >= 80 && numericalGrade < 90) {
+  console.log("B");
+} else if (numericalGrade >= 70 && numericalGrade < 80) {
+  console.log("C");
+} else if (numericalGrade >= 60 && numericalGrade < 70) {
+  console.log("D");
+} else if (numericalGrade >= 50 && numericalGrade < 60) {
+  console.log("E");
+} else if (numericalGrade >= 0 && numericalGrade < 50) {
+  console.log("F");
+} else {
+  return "Invalid grade";
 }
-
-console.log(getLetterGrade(95));
-
-
