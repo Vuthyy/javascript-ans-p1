e5ca6c6db9e92746a5e3aa1e62969769cad06295
Optimusthy.dev <141915285+Vuthyy@users.noreply.github.com>
2024-07-04 09:51:04 +0700

    Update index-06.js

--- index-06.js
+++ index-06.js
@@ -2,12 +2,13 @@ let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let oddCount = 0;
 let evenCount = 0;
 
-numbers.forEach((num) => {
-  if (num % 2 === 0) {
-    evenCount++;
-  } else {
-    oddCount++;
-  }
-});
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        evenCount++;
+    } else {
+        oddCount++;
+    }
+}
 
-console.log(`Odd numbers: ${oddCount}, Even numbers: ${evenCount}`);
+console.log(`Even numbers count: ${oddCount}`);
+console.log(`Odd numbers count: ${evenCount}`);
